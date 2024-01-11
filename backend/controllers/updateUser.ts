00b3afc269b8081bbad07beb64b71c6ebd2764e8
cunhatalisca <gabrielvlog11@gmail.com>
2024-01-11 16:01:44 -0300

    feat: add updateUser

---
+++ backend/controllers/updateUser.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import prisma from '../prismaConfig';
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { id, name, email } = req.body;
+
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id },
+            data: { name, email },
+        });
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to update user' });
+    }
+};
+
