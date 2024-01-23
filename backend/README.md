d7bed0781aae1b210b357d244ae12cd31efb46eb
Laisa Pereira <laisasilva.pereira@yahoo.com>
2024-01-23 11:54:38 -0300

    add: contador e lib para metricas do codigo

--- backend/README.md
+++ backend/README.md
@@ -1,4 +1,1 @@
-## TypeScript starter for NodeJS projects
-
-run `npm ci` or `npm i` based on your preference.
-There are few pre-set npm commands available, take a look at `package.json` scripts.
+## 
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -4,10 +4,19 @@ import { loadAndNormalizeDocuments } from "./documentLoader";
 import { OpenAI } from "langchain/llms/openai";
 import { RetrievalQAChain } from "langchain/chains";
 
+import now from "performance-now";
+
+let totalInteractions = 0;
+let resolvedInteractions = 0;
+let totalTimeSpent = 0;
+
 export const router = Router();
 
 router.post('/', async (request: Request, response: Response) => {
     const { chats } = request.body;
+
+    const startTime = now();
+
     const normalizedDocs = await loadAndNormalizeDocuments();
     const vectorStore = await setupVectorStore(normalizedDocs);
 
@@ -23,5 +32,26 @@ router.post('/', async (request: Request, response: Response) => {
     console.log("Querying chain...");
     const result = await chain.call({ query: chats });
 
+    // metricas 
+    const endTime = now();
+    const elapsedTime = endTime - startTime;
+
+    totalInteractions++;
+    totalTimeSpent += elapsedTime;
+
+    if (result) {
+        resolvedInteractions++;
+    }
+
+    logMetrics();
+
     response.json({ output: result });
 });
+
+// Função para registrar métricas
+function logMetrics() {
+  console.log(`Total Interactions: ${totalInteractions}`);
+  console.log(`Resolved Interactions: ${resolvedInteractions}`);
+  console.log(`Total Time Spent: ${totalTimeSpent} ms`);
+}
+
