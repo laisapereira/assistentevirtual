79a6da19ba6a51a30c4ec5202d8167fa8f89ad97
Laisa Pereira <laisadsilva.pereira@gmail.com>
2024-01-30 10:20:56 -0300

    add: verificação por rotas funcionando

--- backend/src/services/controller/UserController/AuthController.ts
+++ backend/src/services/controller/UserController/AuthController.ts
@@ -7,34 +7,30 @@ const AuthController = async (req: Request, res: Response) => {
 
   const {email, password} = req.body;
   
+  
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+    if (!user) {
+    return res.status(404).json({ message: "Usuário não encontrado" });
+  }
+  
   try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-     if (!user) {
-      return res.status(404).json({ message: "Usuário não encontrado" });
-    }
-  
-    try {
-        const isValuePassword = await bycrypt.compare(password, user.password);
-        if (!isValuePassword) {
-          return res.status(401).json({ error: "Senha incorreta" });
-        }
-        const token = jwt.sign({ id: user.id }, "secret", {expiresIn: "1d"}) // so a aplicacao sabe o secret 
-        return res.json({user, token});
-
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Identificação de senha correspondente falhou" });
+      const isValuePassword = await bycrypt.compare(password, user.password);
+      if (!isValuePassword) {
+        return res.status(401).json({ error: "Senha incorreta" });
       }
-  
-  } catch (error) {
-    console.error(error);
-  
-  }
-  
-  }
+      const token = jwt.sign({ id: user.id }, "secret", {expiresIn: "1d"}) // so a aplicacao sabe o secret 
+      const {id} = user
+      return res.json({user: {id, email}, token});
 
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Senha incorreta" });
+      }
+}
+        
+    
   export default AuthController 
