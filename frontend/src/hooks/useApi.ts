1f427a75e4392f735a892bcd81cfdfa06372480f
Laisa Pereira <laisadsilva.pereira@gmail.com>
2024-02-06 15:49:53 -0300

    add: função logout funcionando devidamente

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -2,18 +2,16 @@ import axios from "axios";
 import mockUsers from "../types/User.ts";
 import Cookies from "js-cookie";
 
-
 const api = axios.create({
   baseURL: process.env.REACT_APP_API,
 });
 
 export const useApi = () => ({
   validateToken: async (token: string) => {
-    
- /*  const response = await api.post("/validateToken", { token }); 
+    /*  const response = await api.post("/validateToken", { token }); 
   return response.data; 
  */
-  const user = mockUsers.find((user) => user.id === +token); // simulando que o token é o ID do usuário
+    const user = mockUsers.find((user) => user.id === +token); // simulando que o token é o ID do usuário
     if (user) {
       return { success: true, user, token };
     } else {
@@ -22,37 +20,22 @@ export const useApi = () => ({
   },
 
   signIn: async (email: string, password: string) => {
-    
- try {
+    try {
       const response = await api.post("/signIn", { email, password });
       console.log(response.data);
 
-      const token = response.data.token;
-      Cookies.set("authToken", token)
+      const token = response.data.token as string;
+      Cookies.set("authToken", token);
       return response.data;
     } catch (error) {
-      console.error('Error signing in:', error);
-      throw error; // Re-throw the error so it can be handled by the caller
+      console.error("Error signing in:", error);
+      throw error;
     }
- 
-   const user = mockUsers.find((user) => user.email === email && user.password === password);
-
-    if (user) {
-      return {
-        user,
-        token: user?.id
-      };
-    } else {
-      return { success: false, message: "Usuário ou senha incorretos" };
-    }
-
   },
 
   logout: async () => {
-    
-    const response = await api.post("/logout");
-    return response.data; 
-
+    Cookies.remove("authToken");
+    window.location.reload()
     return { success: true };
   },
 });
