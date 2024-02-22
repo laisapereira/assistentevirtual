dee25f567dc2919de7e5b86ab6605b78e75ec029
Laisa Pereira <laisadsilva.pereira@gmail.com>
2024-02-22 16:15:06 -0300

    add: adicionando react hook form e yup como schema de formulario

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,16 +1,33 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/Auth/AuthContext.tsx";
+import { yupResolver } from "@hookform/resolvers/yup"
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup"
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+}).required();
+
+interface ILoginInput {
+  email: string;
+  password: string;
+
+}
 export const Login = () => {
   const auth = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({resolver: yupResolver(schema)});
 
   const navigate = useNavigate();
 
-  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
+  const onSubmit = async (data: ILoginInput) => {
+    const { email, password } = data;
     if (email && password) {
       const isLogged = await auth.signIn(email, password); // chamando função require (auth) [que vem do AuthContext], que chama o customhook api pra fazer a requisição no db
       if (isLogged) {
@@ -24,22 +41,26 @@ export const Login = () => {
     <div>
       <h1>Login</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          {...register("email", { required: true, maxLength: 50 })}
         />
+        <p className="text-main-purple">{errors.email?.message}</p>
         <input
           type="password"
           placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          {...register("password", {
+            required: true,
+            maxLength: 20,
+            minLength: 8,
+          })}
         />
-        <button type="submit" onClick={handleLogin}>
-          Entrar
-        </button>
+        <p>{errors.password?.message}</p>
+
+       {/*  {errors.email && errors.password && <span>This field is required</span>} */}
+        <button type="submit">Entrar</button>
       </form>
     </div>
   );
