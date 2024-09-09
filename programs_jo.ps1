2fef10fd2527399b8a8c6302ac06ebc198233852
Laisa Pereira <laisa.pereira.91499@gmail.com>
2024-09-09 14:26:48 -0300

    add programs.ps1

---
+++ programs_jo.ps1
@@ -0,0 +1,18 @@
+# Instala programas via Chocolatey
+
+choco install visualstudio2022community
+
+choco install python -y
+choco python -m pip install -U pip
+
+choco install vcredist-all
+
+choco install vscode 
+
+choco install git.install
+choco install nodejs.install
+choco install docker-desktop -y
+
+choco install postgresql
+
+choco install pgadmin4
