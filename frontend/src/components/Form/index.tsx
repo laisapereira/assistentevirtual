ea8d7b5cc978f5bd353339e80a8b61a0cedc409e
Arthur Barreiros <arthurbarreiros.dev@gmail.com>
2024-12-20 12:41:28 -0300

    gipojs

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -9,13 +9,6 @@ interface ChatFormProps {
 const ChatForm: React.FC<ChatFormProps> = ({ onSubmit }) => {
   const [input, setInput] = useState("");
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputField = e.target as HTMLTextAreaElement;
-    inputField.style.height = "auto"; // Reseta a altura para ajustar novamente
-    inputField.style.height = `${inputField.scrollHeight}px`; // Ajusta a altura dinamicamente
-    setInput(inputField.value); // Atualiza o estado com o valor digitado
-  };
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (input.trim() !== "") {
@@ -26,17 +19,17 @@ const ChatForm: React.FC<ChatFormProps> = ({ onSubmit }) => {
 
   return (
     <form onSubmit={handleSubmit} className="form-submit">
-      <textarea
-        id="user-input"
-        name="user-input"
-        placeholder="Digite sua dúvida..."
-        value={input}
-        onChange={handleInputChange}
-        rows={1} // Altura inicial do campo de texto
-        style={{ overflow: "hidden", resize: "none" }} // Evita redimensionamento manual
-      />
-      <button type="submit">Enviar</button>
-    </form>
+  <textarea
+    id="user-input"
+    name="user-input"
+    placeholder="Digite sua dúvida..."
+    value={input}
+    onChange={(e) => setInput(e.target.value)}
+    rows={3} // Número de linhas iniciais
+  />
+  <button type="submit">Enviar</button>
+</form>
+
   );
 };
 
